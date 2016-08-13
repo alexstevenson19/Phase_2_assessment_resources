da18112282a35210cf355c9106bb2d3e36cc1329
Alex S <email": null>
2016-08-13 17:16:07 -0500

    add phase 2 assessment practice controller

--- ajax_control_skeleton.rb
+++ ajax_control_skeleton.rb
@@ -1,3 +1,34 @@
+# GB assessment relevant controllers =====================================
+
+post "/posts" do
+  if request.xhr?
+    p "XHR=========================================="
+    # p params[:post]
+    new_post = Post.create(params[:post])
+    # p new_post
+    # we need to send this to get its layout, to then append, perpend, insert... into the static page
+    erb :'/posts/_post', layout: false, locals: {post: new_post}
+  else
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect "posts/#{@post.id}"
+    end
+  end
+end
+
+
+put "/posts/:id/like" do
+  @post = Post.find(params[:id])
+  @post.increment!(:likes_count)
+  if request.xhr?
+    p "XHR============================================="
+    # just need to send back the number
+    @post.likes_count.to_s
+  else
+    redirect "/posts/#{@post.id}"
+  end
+end
+
 
 # Hacker News relevant controllers ========================================
 post '/posts/:id/vote' do
